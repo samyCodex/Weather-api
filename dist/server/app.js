51e41a1406ba4e4bce6df7f043e0caf81ce2c947
samson onifade <gbemilekeadeboyega@gmail.com>
2025-03-01 01:32:43 +0100

    Deployment 2

--- dist/server/app.js
+++ dist/server/app.js
@@ -7,12 +7,22 @@ const express_1 = __importDefault(require("express"));
 const index_1 = __importDefault(require("../routes/index"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+const whitelist = ['http://example1.com', 'http://example2.com'];
+const corsOptionsDelegate = function (req, callback) {
+    let corsOptions;
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    }
+    else {
+        corsOptions = { origin: false }; // disable CORS for this request
+    }
+    callback(null, corsOptions); // callback expects two parameters: error and options
+};
 app.use(express_1.default.json());
-app.get('/', (req, res) => {
+app.get('/', (0, cors_1.default)(corsOptionsDelegate), (req, res) => {
     res.send('Server is Online');
 });
-app.use('/api', index_1.default);
+app.use('/api', (0, cors_1.default)(corsOptionsDelegate), index_1.default);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Server is running on port http://localhost:' + PORT);
--- src/server/app.ts
+++ src/server/app.ts
@@ -3,12 +3,21 @@ import IndexRouter from '../routes/index';
 import cors from 'cors'
 const app: Application = express();
 
-app.use(cors());
+const whitelist = ['http://localhost:5174', 'https://illustrious-platypus-f7576f.netlify.app']
+const corsOptionsDelegate = function (req, callback) {
+let corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 app.use(express.json());
-app.get('/', (req: Request, res: Response) => {
+app.get('/', cors(corsOptionsDelegate), (req: Request, res: Response) => {
     res.send('Server is Online');
 });
-app.use('/api', IndexRouter);
+app.use('/api', cors(corsOptionsDelegate), IndexRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
